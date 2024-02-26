1d7f1647821f38dbf4a6bfc249d4537df3c64faf
Erick Hitter <ehitter@pmc.com>
2024-02-26 09:08:52 -0800

    Add support for `pmc-custom-feed-v2` (#10)

---
+++ classes/query-args/class-pmc-custom-feed.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Retain `pmc-custom-feed` objects.
+ *
+ * @package pmc-wp-local-data-cli
+ */
+
+declare( strict_types = 1 );
+
+namespace PMC\WP_Local_Data_CLI\Query_Args;
+
+use PMC\WP_Local_Data_CLI\Query_Args;
+use PMC_Custom_Feed as Plugin;
+
+/**
+ * Class PMC_Custom_Feed.
+ */
+final class PMC_Custom_Feed extends Query_Args {
+	/**
+	 * Skip processing custom feed objects since they have no dependencies.
+	 *
+	 * @var bool
+	 */
+	public static bool $find_linked_ids = false;
+
+	/**
+	 * Whether or not to skip the backfill process. Typically this is used when
+	 * a query's `post_type` is set to the special value "any" as backfill
+	 * cannot be applied to such a query.
+	 *
+	 * @var bool
+	 */
+	public static bool $skip_backfill = true;
+
+	/**
+	 * Build array of `WP_Query` arguments used to retrieve IDs to retain.
+	 *
+	 * @return array
+	 */
+	public static function get_query_args(): array {
+		return [
+			'post_type'      => Plugin::post_type_name,
+			'posts_per_page' => -1,
+		];
+	}
+}
